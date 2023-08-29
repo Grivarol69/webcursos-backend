f448ac33813d8ac83c08a4ddfa8f6e96002d5c0f
Guillermo Rivarola <grivarol69@gmail.com>
2023-08-29 17:54:18 -0500

    agregamos Routing de Usuarios

---
+++ src/routes/UserRouter.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { saludar } from '../controllers/userController.js';
+
+const userRouter = Router();
+
+userRouter.get('/saludar', saludar);
+
+export default userRouter;
