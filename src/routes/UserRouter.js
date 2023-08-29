af22367f937289c646033a51dd0267c17e278caf
Guillermo Rivarola <grivarol69@gmail.com>
2023-08-29 18:17:54 -0500

    cambios varios

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -3,6 +3,7 @@ import { saludar } from '../controllers/userController.js';
 
 const userRouter = Router();
 
+
 userRouter.get('/saludar', saludar);
 
 export default userRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import userRouter from "./UserRouter.js";
 
 const router = Router();
 
+
 router.use('/api/users', userRouter);
 
 export default router;
